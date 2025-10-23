665b05688436f006a2963a7148f94b328b683e5b
ctottinger <christiantannerottinger@gmail.com>
2025-10-23 18:26:22 -0400

    Updated for week 8 standings

--- src/standings/WeekSeven.tsx
+++ src/standings/WeekSeven.tsx
@@ -8,27 +8,27 @@ export const WeekSevenStandings = {
                     rank: 1,
                     name: "BigSwagooo713",
                     divisionRecord: "1-1",
-                    conferenceRecord: "1-1",
-                    overallRecord: "4-2",
-                    pointsFor: 748.06,
+                    conferenceRecord: "2-1",
+                    overallRecord: "5-2",
+                    pointsFor: 882,
                     icon: "photo-1582562124811-c09040d0a901"
                 },
                 {
                     rank: 2,
                     name: "Touchdown Syndrome",
-                    divisionRecord: "0-1",
+                    divisionRecord: "0-2",
                     conferenceRecord: "0-3",
-                    overallRecord: "1-5",
-                    pointsFor: 725.58,
+                    overallRecord: "1-6",
+                    pointsFor: 809.8,
                     icon: "photo-1618160702438-9b02ab6515c9"
                 },
                 {
                     rank: 3,
                     name: "ctottinger",
-                    divisionRecord: "1-0",
+                    divisionRecord: "2-0",
                     conferenceRecord: "2-0",
-                    overallRecord: "4-2",
-                    pointsFor: 742.96,
+                    overallRecord: "5-2",
+                    pointsFor: 978.8,
                     icon: "photo-1466721591366-2d5fba72006d"
                 }
             ]
@@ -39,28 +39,28 @@ export const WeekSevenStandings = {
                 {
                     rank: 1,
                     name: "BigRedAlec",
-                    divisionRecord: "1-0",
+                    divisionRecord: "1-1",
                     conferenceRecord: "2-2",
-                    overallRecord: "4-2",
-                    pointsFor: 827.42,
+                    overallRecord: "4-3",
+                    pointsFor: 948.84,
                     icon: "photo-1535268647677-300dbf3d78d1"
                 },
                 {
                     rank: 2,
                     name: "1st & Won",
-                    divisionRecord: "0-1",
+                    divisionRecord: "1-1",
                     conferenceRecord: "1-2",
-                    overallRecord: "1-5",
-                    pointsFor: 687.02,
+                    overallRecord: "2-5",
+                    pointsFor: 814.42,
                     icon: "photo-1452960962994-acf4fd70b632"
                 },
                 {
                     rank: 3,
-                    name: "YungChepe13",
+                    name: "Jack Goff", // YungChepe13
                     divisionRecord: "1-1",
-                    conferenceRecord: "1-0",
-                    overallRecord: "3-3",
-                    pointsFor: 748.92,
+                    conferenceRecord: "1-1",
+                    overallRecord: "3-4",
+                    pointsFor: 853.66,
                     icon: "photo-1582562124811-c09040d0a901"
                 }
             ]
@@ -73,29 +73,29 @@ export const WeekSevenStandings = {
             teams: [
                 {
                     rank: 1,
-                    name: "Call me maye-be",
-                    divisionRecord: "1-0",
+                    name: "Call me maye-be", // papaboeing
+                    divisionRecord: "1-1",
                     conferenceRecord: "2-1",
-                    overallRecord: "5-1",
-                    pointsFor: 871.98,
+                    overallRecord: "5-2",
+                    pointsFor: 1022.94,
                     icon: "photo-1618160702438-9b02ab6515c9"
                 },
                 {
                     rank: 2,
-                    name: "Creatine Caucasians",
-                    divisionRecord: "0-1",
+                    name: "Creatine Caucasians", // Phenomenal10
+                    divisionRecord: "1-1",
                     conferenceRecord: "3-0",
-                    overallRecord: "5-1",
-                    pointsFor: 847.88,
+                    overallRecord: "6-1",
+                    pointsFor: 1010.14,
                     icon: "photo-1466721591366-2d5fba72006d"
                 },
                 {
                     rank: 3,
-                    name: "$uperCam & Smittys 1ceBox",
+                    name: "$uperCam & Smittys 1ceBox", // jmitchell979
                     divisionRecord: "1-1",
-                    conferenceRecord: "0-1",
-                    overallRecord: "2-4",
-                    pointsFor: 762.14,
+                    conferenceRecord: "1-1",
+                    overallRecord: "3-4",
+                    pointsFor: 926.84,
                     icon: "photo-1535268647677-300dbf3d78d1"
                 }
             ]
@@ -106,28 +106,28 @@ export const WeekSevenStandings = {
                 {
                     rank: 1,
                     name: "knowsballs",
-                    divisionRecord: "1-0",
+                    divisionRecord: "1-1",
                     conferenceRecord: "0-2",
-                    overallRecord: "2-4",
-                    pointsFor: 672.96,
+                    overallRecord: "2-5",
+                    pointsFor: 778.3,
                     icon: "photo-1452960962994-acf4fd70b632"
                 },
                 {
                     rank: 2,
                     name: "JockstrapJorker",
-                    divisionRecord: "1-0",
+                    divisionRecord: "2-0",
                     conferenceRecord: "1-2",
-                    overallRecord: "3-3",
-                    pointsFor: 723.18,
+                    overallRecord: "4-3",
+                    pointsFor: 857.32,
                     icon: "photo-1582562124811-c09040d0a901"
                 },
                 {
                     rank: 3,
                     name: "8igDonDada",
                     divisionRecord: "0-2",
-                    conferenceRecord: "1-1",
-                    overallRecord: "2-4",
-                    pointsFor: 690.72,
+                    conferenceRecord: "1-2",
+                    overallRecord: "2-5",
+                    pointsFor: 795.2,
                     icon: "photo-1618160702438-9b02ab6515c9"
                 }
             ]
